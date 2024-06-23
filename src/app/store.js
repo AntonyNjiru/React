3cea84a91836a52f0d62b83bcd9ff0ab28ba37ed
AntonyNjiru <kantony.njiru@gmail.com>
2024-06-23 20:57:22 +0300

    updates

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {campsitesReducer} from '../features/campsites/CampsitesSlice';
+import { userReducer } from './userSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+      user: userReducer,
+      counter: campsitesReducer,
+  }
 });
