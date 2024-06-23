5f67f2b7650c35a7492346164884345087b39988
AntonyNjiru <kantony.njiru@gmail.com>
2024-06-23 21:59:30 +0300

    week 5 workshop

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ContactPage from './pages/ContactPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
+//import React from 'react';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import { fetchCampsites } from "./features/campsites/CampsitesSlice";
+import { fetchPartners } from "./features/partners/partnersSlice";
+import { fetchPromotions } from "./features/promotions/promotionsSlice";
+
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <Routes>
-            <Route
-                    path='directory/:campsiteId'
-                    element={<CampsiteDetailPage />}
-                />
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-            </Routes>
-            <Footer />
-        </div>
-    );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions());
+  }, [dispatch]);
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/features/campsites/comments/CommentsList.js
+++ src/features/campsites/comments/CommentsList.js
@@ -1,29 +1,32 @@
-import { Col } from 'reactstrap';
-import Comment from './Comment';
-import { selectCommentsByCampsiteId } from './commentsSlice';
+import { useSelector } from "react-redux";
+import { Col } from "reactstrap";
+import Comment from "./Comment";
+import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
+
 import React from "react";
 
 const CommentsList = ({ campsiteId }) => {
-    const comments = selectCommentsByCampsiteId(campsiteId);
-    if (comments && comments.length > 0) {
-      return (
-        <Col md="5" className="m-1">
-          <h4>Comments</h4>
-          {comments.map((comment) => {
-            return <Comment key={comment.id} comment={comment} />;
-          })}
-          <CommentForm campsiteId={campsiteId} />
-  
-        </Col>
-      );
-    }
-    return(
-      <Col md='5' and className='m-1'>
-          There are no comments for this campsite yet.
+  const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+  if (comments && comments.length > 0) {
+    return (
+      <Col md="5" className="m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return <Comment key={comment.id} comment={comment} />;
+        })}
+        {/* campsiteId as a prop */}
+        <CommentForm campsiteId={campsiteId} />
+
       </Col>
     );
-   
-  };
-  
-  export default CommentsList;
+  }
+  return(
+    <Col md='5' and className='m-1'>
+        There are no comments for this campsite yet.
+    </Col>
+  );
+ 
+};
+
+export default CommentsList;
